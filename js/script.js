546b3467d0c426b3509668e2c9baa027e1d657f2
Nurali Davlatov <nur3.dav.97@gmail.com>
2023-08-27 03:20:52 +0500

    modal

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-// var scene = document.getElementById('scene');
-// 		var parallaxInstance = new Parallax(scene, {
-// 			relativeInput: true
-// 		});
-// var scene2 = document.getElementById('scene2');
-// 		var parallaxInstance = new Parallax(scene2, {
-// 			relativeInput: true
-// 		});
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene, {
+	relativeInput: true
+});
+
+var scene2 = document.getElementById('scene2');
+var parallaxInstance = new Parallax(scene2, {
+	relativeInput: true
+});
+
+const btn_open = document.getElementById('btn_open'),
+	modal_close = document.getElementById('modal_close'),
+	modal = document.getElementById('modal');
+
+	console.log(modal);
+
+modal_close.onclick = () => { modal.classList.remove('active') }
+btn_open.onclick = () => { modal.classList.add('active') }
