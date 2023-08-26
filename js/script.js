37e4c9ae61e6d1ec13d3590a937966d8dbe479b0
Nurali Davlatov <nur3.dav.97@gmail.com>
2023-08-26 21:56:16 +0500

    tablet

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-var scene = document.getElementById('scene');
-		var parallaxInstance = new Parallax(scene, {
-			relativeInput: true
-		});
-var scene2 = document.getElementById('scene2');
-		var parallaxInstance = new Parallax(scene2, {
-			relativeInput: true
-		});
+// var scene = document.getElementById('scene');
+// 		var parallaxInstance = new Parallax(scene, {
+// 			relativeInput: true
+// 		});
+// var scene2 = document.getElementById('scene2');
+// 		var parallaxInstance = new Parallax(scene2, {
+// 			relativeInput: true
+// 		});
